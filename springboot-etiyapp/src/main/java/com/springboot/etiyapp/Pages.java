70ef3c9e6401163d4da7fafa5163ae40b4714c56
Faruk <32228542+omermarvel@users.noreply.github.com>
2021-09-29 02:32:18 +0300

    Website Remake

--- springboot-etiyapp/src/main/java/com/springboot/etiyapp/Pages.java
+++ springboot-etiyapp/src/main/java/com/springboot/etiyapp/Pages.java
@@ -14,5 +14,27 @@ public class Pages {
 		model.addAttribute("name", name);
 		return "index";
 	}
+	
+	@GetMapping("/insert/city")
+	public String CitySite(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "city";
+	}
+	
+	@GetMapping("/insert/town")
+	public String TownSite(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "town";
+	}
+	@GetMapping("/insert/branch")
+	public String BranchSite(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "branch";
+	}
+	@GetMapping("/insert/employee")
+	public String EmployeeSite(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "employee";
+	}
 
 }
